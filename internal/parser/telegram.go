da13d93bf77cf5f7b02f69de68b4d013148599a8
SkobelkinYaroslav <yarl@duck.com>
2024-07-05 19:14:21 +0400

    updated telegram parser: better error handling

--- internal/parser/telegram.go
+++ internal/parser/telegram.go
@@ -14,14 +14,22 @@ type TelegramParser struct {
 }
 
 func NewTelegramParser(channels []string) TelegramParser {
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"),
+	)
+
+	// Устанавливаем задержку между запросами
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Delay:       5 * time.Second,
+		RandomDelay: 1 * time.Second,
+	})
+
 	return TelegramParser{
-		engine: colly.NewCollector(
-			colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"),
-		),
+		engine:   c,
 		channels: channels,
 	}
 }
-
 func (p TelegramParser) Telegram() []result.Result {
 	now := time.Now()
 	startOfToday := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
@@ -33,7 +41,7 @@ func (p TelegramParser) Telegram() []result.Result {
 
 		html, err := e.DOM.Find(".tgme_widget_message_text").Html()
 		if err != nil {
-			log.Printf("Error getting HTML: %v\n", err)
+			log.Printf("Error getting HTML from %s: %v", e.Request.URL, err)
 			return
 		}
 
@@ -46,12 +54,11 @@ func (p TelegramParser) Telegram() []result.Result {
 		localTime := parsedDateTime.Local()
 
 		if err != nil {
-			log.Printf("Error parsing datetime: %v\n", err)
+			log.Printf("Error parsing datetime %v:%v\n", localTime, err)
 			return
 		}
 
 		if localTime.Before(startOfToday) {
-			//log.Println("Post is outdated:", localTime)
 			return
 		}
 
@@ -60,9 +67,17 @@ func (p TelegramParser) Telegram() []result.Result {
 		responses = append(responses, curPost)
 	})
 
+	p.engine.OnError(func(r *colly.Response, err error) {
+		log.Printf("Error while sending request to %s: %v", r.Request.URL, err)
+		time.Sleep(10 * time.Second)
+		if err := r.Request.Retry(); err != nil {
+			log.Printf("Error while retrying request to %s: %v", r.Request.URL, err)
+			return
+		}
+	})
+
 	for _, channel := range p.channels {
 		p.engine.Visit(channel)
-		time.Sleep(3 * time.Second)
 	}
 
 	return responses
