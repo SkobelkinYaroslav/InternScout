2b6381eaaaca0f6ebe507568c21c9c9643553cf2
SkobelkinYaroslav <yarl@duck.com>
2024-07-16 21:11:01 +0400

    small statistics at the end

--- cmd/yarl_intern_bot/main.go
+++ cmd/yarl_intern_bot/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
+	"time"
 	"yarl_intern_bot/internal/parser"
 	"yarl_intern_bot/internal/readFile"
 	"yarl_intern_bot/internal/sendResults"
@@ -9,6 +11,7 @@ import (
 )
 
 func main() {
+	now := time.Now()
 	err := godotenv.Load(".env")
 
 	if err != nil {
@@ -16,7 +19,7 @@ func main() {
 	}
 
 	// read channels list
-	channels := readFile.GetChannels()
+	channels := readFile.GetChannels("channels.txt")
 
 	//get users and their settings
 	users := user.New("config.json")
@@ -30,5 +33,7 @@ func main() {
 
 	// send results to users
 	sendResults.Telegram(users)
+
+	fmt.Printf("%d posts were processed in %.3f", len(results), time.Since(now).Minutes())
 
 }
